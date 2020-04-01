da8a9155f2e79c4f88b7661a281903ee175d6bd3
Vitaly <monstr.by@gmail.com>
2020-04-01 22:15:54 +0300

    Create main project files

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+// var LeftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+// console.log(4/0);
+// console.log('somesring'*4);
+
+// let Persone = {
+//     name: "Alex",
+//     age: 30,
+//     isMarried: false
+// };
+
+// console.log(Persone);
+// console.log(Persone.name);
+
+// let arr = ["first.png","second.png","third.png"];
+// console.log(arr[1]);
+
+// alert ("Hello bitches!")
+// let answer = confirm ("Anybody home ye?");
+// console.log(answer);
+
+//  let answer = +prompt ("Anybody home ye?", "No");
+// console.log(typeof(answer));
+
+
+// console.log("One hamster, " + "Two hamster");
+// console.log(5 + " Two hamster");
+
+// let incr = 10,
+//     decr = 10;
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(13%6);
+
+// console.log("3" === 3);
+
+let isChecked = true,
+    isClosed = false;
+
+console.log(isChecked || isClosed);
+
