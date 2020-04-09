14b69e47c4702c38854822eaa2b3d2a3c11b6d02
Vitaly <monstr.by@gmail.com>
2020-04-09 23:26:00 +0300

    Write simple function and learning any methods

--- test.js
+++ test.js
@@ -27,23 +27,23 @@ let appData = {
 
 
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt ("Введите обязательную статью расходов в этом месяце");
-    let b = prompt ("Во сколько обойдется?");
+// for (let i = 0; i < 2; i++) {
+//     let a = prompt ("Введите обязательную статью расходов в этом месяце");
+//     let b = prompt ("Во сколько обойдется?");
     
-    if ( (typeof(a)) === "string" && (typeof(a)) != null && 
-    (typeof(b)) != null && a != '' && b != '' && a.length < 50 )
-    {
-    appData.expenses[a] = b;
-    console.log('done');
-    }
-    else {
-        console.log ("bad result");
-        i--;
-    }
-};
+//     if ( (typeof(a)) === "string" && (typeof(a)) != null && 
+//     (typeof(b)) != null && a != '' && b != '' && a.length < 50 )
+//     {
+//     appData.expenses[a] = b;
+//     console.log('done');
+//     }
+//     else {
+//         console.log ("bad result");
+//         i--;
+//     }
+// };
 
-// Второй способ через while
+// // Второй способ через while
 
 
 // let i = 0;
@@ -87,19 +87,73 @@ for (let i = 0; i < 2; i++) {
 // while(i < 2);
 
 
-appData.moneyPerDay = appData.budget / 30;
+// appData.moneyPerDay = appData.budget / 30;
 
-console.log(appData.expenses);
+// console.log(appData.expenses);
 
-let oneDayCost = alert("Ваш бюджет на 1 день равен " + appData.moneyPerDay);
-console.log(oneDayCost);
+// let oneDayCost = alert("Ваш бюджет на 1 день равен " + appData.moneyPerDay);
+// console.log(oneDayCost);
 
-if (appData.moneyPerDay < 100) {
-    console.log("Низкий уровень достатка!");
-} else if (appData.moneyPerDay > 200 && appData.moneyPerDay < 1000) {
-    console.log('Средний уровень достатка!');
-} else if (appData.moneyPerDay > 1000) {
-    console.log('Высокий уровень достатка!');
-} else {
-    console.log('Произошла ошибка!');
+// if (appData.moneyPerDay < 100) {
+//     console.log("Низкий уровень достатка!");
+// } else if (appData.moneyPerDay > 200 && appData.moneyPerDay < 1000) {
+//     console.log('Средний уровень достатка!');
+// } else if (appData.moneyPerDay > 1000) {
+//     console.log('Высокий уровень достатка!');
+// } else {
+//     console.log('Произошла ошибка!');
+// }
+
+// Simple function
+/////////////////////////////////////////////////////////////////////////////////
+// let num = 30;
+// function showFirstMessage (text) {
+//     alert(text);
+//     let num = 14;
+//     console.log(num);
+// }
+// console.log(num);
+
+// showFirstMessage("First message from function");
+// Function declaration - может выполняться до самого кода функции
+// console.log(calc(5,7));
+
+// console.log(calc(100,150));
+// function calc (a,b) {
+//     return(a +b);
+// }
+
+
+function retVar () {
+    let alpha = 5;
+    return alpha;
 }
+
+let beta = retVar();
+console.log(beta);
+
+//Function expression выполняется только после кода функции
+
+let summ = function (a,b) {
+    return(a+b);
+};
+
+console.log(summ(3,7));
+
+//ES6 standart стрелочная функция
+let summES6 = (a,b) => a+b;
+console.log(summES6(1,2));
+
+let str = "anakonda";
+console.log(str.length);
+console.log(str.toUpperCase()); //метод переводит к верхнему регистру строку
+console.log(str.toLowerCase()); //метод переводит к нижнему регистру
+
+let twelwe = "12.2px";
+//console.log(Math.round(twelwe));
+console.log(parseInt(twelwe)); // метод округляет и переводит в целое число
+console.log(parseFloat(twelwe)); // метод переводит в десятичное число
+
+
+
+
